bdeda43b7f3969ac37bae08db2dd8ed6eb70f3ea
Pichaya Punyakhetpimook <pichayapu@sanook.com>
2015-12-03 19:34:38 +0700

    Add /templates

---
+++ templates/index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+        <title>My Template</title>
+    </head>
+
+  	<body>
+      <h1>
+        My Themes
+      </h1>
+  		<p>
+  			<ul>
+          <li><a href="404_error_bs3/">404_error_bs3</a></li>
+          <li><a href="agency_bs3/">agency_bs3</a></li>
+          <li><a href="agency_bs3_dark/">agency_bs3_dark</a></li>
+          <li><a href="blog_article_bs3/">blog_article_bs3</a></li>
+          <li><a href="blog_category_bs3/">blog_category_bs3</a></li>
+          <li><a href="bookingdirectory/">bookingdirectory</a></li>
+          <li><a href="BootScroller_bs3/">BootScroller_bs3</a></li>
+          <li><a href="contact_bs3/">contact_bs3</a></li>
+          <li><a href="events/">events</a></li>
+          <li><a href="FLAT_STYLE_BLOG/">FLAT_STYLE_BLOG</a></li>
+          <li><a href="FlatLand/">FlatLand</a></li>
+          <li><a href="indexbusinessinfo_bs3_plus/">indexbusinessinfo_bs3_plus</a></li>
+          <li><a href="indexpage_bs3_plus/">indexpage_bs3_plus</a></li>
+          <li><a href="indexshop_bs3/">indexshop_bs3</a></li>
+          <li><a href="LANDING_DARK_CLASSIC/">LANDING_DARK_CLASSIC</a></li>
+          <li><a href="LANDING_DARK_FLAT/">LANDING_DARK_FLAT</a></li>
+          <li><a href="landing_page_bs3/">landing_page_bs3</a></li>
+          <li><a href="LANDING_WHITE_FLATTY/">LANDING_WHITE_FLATTY</a></li>
+          <li><a href="parallaxer_bs3/">parallaxer_bs3</a></li>
+          <li><a href="pinboard_bs3/">pinboard_bs3</a></li>
+          <li><a href="portal-main/">portal-main</a></li>
+          <li><a href="portfolio_bs3/">portfolio_bs3</a></li>
+          <li><a href="project_img_bs3/">project_img_bs3</a></li>
+          <li><a href="project_video_bs3/">project_video_bs3</a></li>
+          <li><a href="RealEstate/">RealEstate</a></li>
+          <li><a href="Services/">Services</a></li>
+          <li><a href="SimpleAdmin/">SimpleAdmin</a></li>
+          <li><a href="simpleblog/">simpleblog</a></li>
+          <li><a href="simpleCorpLand/">simpleCorpLand</a></li>
+          <li><a href="Squeeze_pages/">Squeeze_pages</a></li>
+          <li><a href="StoreFront/">StoreFront</a></li>
+          <li><a href="Themes_Pack/">Themes_Pack</a></li>
+  				<li><a href="thumbs/">thumbs</a></li>
+  			</ul>
+  		</p>
+  	</body>
+</html>
